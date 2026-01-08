667aa6298969403a2072e4cbe9eb949acff9bbbe
DepressiveStar228 <artemka.n.arambler.ru@gmail.com>
2026-01-08 04:24:52 +0200

    Лаб 4 Синхронна подвійна фізична реплікація

--- db/replications/publisher/030_enable_sync.sql
+++ db/replications/publisher/030_enable_sync.sql
@@ -1,2 +1,2 @@
 ALTER SYSTEM SET synchronous_commit = 'on';
-ALTER SYSTEM SET synchronous_standby_names = 'traveler_replica';
+ALTER SYSTEM SET synchronous_standby_names = 'ANY 2 (traveler_replica, traveler_replica_2)';
